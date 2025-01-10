282a71ee8ebd17fd75af212654848246273c27b8
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-10 01:21:42 -0500

    Added middleware

--- index.js
+++ index.js
@@ -12,4 +12,33 @@ const users = require("./routes/users");
 const posts = require("./routes/posts");
 
 
+// Middleware to parse incoming requests
+app.use(bodyParser.json({ extended: true })); //This middleware is used to parse incoming JSON data.
+app.use(bodyParser.urlencoded({ extended: true })); //This middleware is used to parse URL-encoded data (like form submissions).
 
+//Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
+
+  
+// Authentication Middleware
+function checkAuth(req, res, next) {
+    const authToken = req.headers['x-auth-token'];
+    if (!authToken) {
+      return res.status(401).json({ message: 'Unauthorized access' });
+    }
+    next();
+  }
+
+
+
+
+// Error handling Middleware
+app.use((req, res) => {
+    res.status(404);
+    res.json({ error: "Resource Not Found" });
+  });
+  
+  
