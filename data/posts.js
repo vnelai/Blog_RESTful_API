f8acbd7de79602cb455dfd508802d402d42b0211
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-10 01:23:14 -0500

    Add post data

---
+++ data/posts.js
@@ -0,0 +1,76 @@
+let posts = [
+    {
+      id: 1,
+      title: 'First Post',
+      content: 'This is the content of my first blog post!',
+      userId: 1, // User ID that created the post
+      comments: [1, 2] // Array of comment IDs associated with this post
+    },
+    {
+      id: 2,
+      title: 'Exploring JavaScript',
+      content: 'Let’s dive into the basics of JavaScript and its key features.',
+      userId: 2,
+      comments: [3, 4]
+    },
+    {
+      id: 3,
+      title: 'A Day in the Life',
+      content: 'Today, I’m sharing a glimpse into my daily routine as a developer.',
+      userId: 3,
+      comments: [5, 6]
+    },
+    {
+      id: 4,
+      title: 'Healthy Eating Tips',
+      content: 'Here are some easy tips for maintaining a healthy diet while working remotely.',
+      userId: 4,
+      comments: [7, 8]
+    },
+    {
+      id: 5,
+      title: 'Why I Love Coding',
+      content: 'Coding is more than just a job for me; it’s my passion.',
+      userId: 5,
+      comments: [9, 10]
+    },
+    {
+      id: 6,
+      title: 'Learning Express.js',
+      content: 'Express.js is a fast, unopinionated, minimalist web framework for Node.js.',
+      userId: 6,
+      comments: [11, 12]
+    },
+    {
+      id: 7,
+      title: 'My Travel Adventures',
+      content: 'Sharing my favorite destinations and travel stories.',
+      userId: 7,
+      comments: [13, 14]
+    },
+    {
+      id: 8,
+      title: 'Tips for Staying Productive',
+      content: 'These are the strategies I use to stay productive while working from home.',
+      userId: 8,
+      comments: [15, 16]
+    },
+    {
+      id: 9,
+      title: 'How to Start Blogging',
+      content: 'A simple guide for anyone looking to start their own blog.',
+      userId: 9,
+      comments: [17, 18]
+    },
+    {
+      id: 10,
+      title: 'Breaking into Tech',
+      content: 'Sharing my journey and tips for transitioning into a tech career.',
+      userId: 10,
+      comments: [19, 20]
+    }
+  ];
+  
+  module.exports = posts;
+
+  
