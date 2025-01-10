a3e53fea8ccaa7e59681d53302385767ff60e40e
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-10 01:22:49 -0500

    Add user data

---
+++ data/users.js
@@ -0,0 +1,14 @@
+let users = [
+    { id: 1, username: 'johndoe', email: 'johndoe@example.com' },
+    { id: 2, username: 'janedoe', email: 'janedoe@example.com' },
+    { id: 3, username: 'mikesmith', email: 'mikesmith@example.com' },
+    { id: 4, username: 'sarahconnor', email: 'sarahconnor@example.com' },
+    { id: 5, username: 'tonystark', email: 'tonystark@example.com' },
+    { id: 6, username: 'natasharomanoff', email: 'natasharomanoff@example.com' },
+    { id: 7, username: 'steverogers', email: 'steverogers@example.com' },
+    { id: 8, username: 'brucewayne', email: 'brucewayne@example.com' },
+    { id: 9, username: 'dianaprince', email: 'dianaprince@example.com' },
+    { id: 10, username: 'clarkkent', email: 'clarkkent@example.com' }
+  ];
+  
+  module.exports = users;
