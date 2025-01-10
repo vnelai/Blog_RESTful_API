8b253c274c450a76cc408dd15b50db6ab1690fd6
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-10 02:48:11 -0500

    Ad comment data

---
+++ data/comments.js
@@ -0,0 +1,62 @@
+[
+    {
+      "id": 1,
+      "userId": 3,
+      "postId": 10,
+      "content": "This post really helped me understand the basics of cloud engineering! Thanks for sharing!"
+    },
+    {
+      "id": 2,
+      "userId": 5,
+      "postId": 10,
+      "content": "Great tips on getting started in tech. I’ll definitely be trying these out."
+    },
+    {
+      "id": 3,
+      "userId": 2,
+      "postId": 7,
+      "content": "I’ve been thinking of transitioning to cybersecurity for a while. This post gave me a lot of motivation."
+    },
+    {
+      "id": 4,
+      "userId": 1,
+      "postId": 7,
+      "content": "Thank you for the clear roadmap. Cybersecurity seems like a great career option!"
+    },
+    {
+      "id": 5,
+      "userId": 6,
+      "postId": 8,
+      "content": "Really enjoyed this article! It’s inspiring to see others go from healthcare to tech."
+    },
+    {
+      "id": 6,
+      "userId": 4,
+      "postId": 5,
+      "content": "I didn’t know about cloud certifications before. I’ll definitely look into this."
+    },
+    {
+      "id": 7,
+      "userId": 8,
+      "postId": 3,
+      "content": "Awesome content! Just started my cloud engineering journey, and this is super helpful."
+    },
+    {
+      "id": 8,
+      "userId": 10,
+      "postId": 2,
+      "content": "This is exactly what I was looking for to help me break into tech!"
+    },
+    {
+      "id": 9,
+      "userId": 7,
+      "postId": 6,
+      "content": "I never thought about using cloud technologies in such creative ways. Great post!"
+    },
+    {
+      "id": 10,
+      "userId": 3,
+      "postId": 4,
+      "content": "The resources you mentioned are perfect for getting into software development. Thanks for the recommendations!"
+    }
+  ]
